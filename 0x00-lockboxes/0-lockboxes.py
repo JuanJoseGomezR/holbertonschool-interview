e2748598b516e3dc2fea18edaad4a3d4f42089e5
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-11 03:24:11 +0000

    [Uploading] task

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+#!/user/bin/python3
+
+def canUnlockAll(boxes):
+	keys = [0]
+	for key in keys:
+		for idx in boxes[key]:
+			if (idx not in keys and idx != key and idx < len(boxes) and idx != 0):
+				keys.append(idx)
+	if len(keys) == len(boxes):
+		return True
+	return False
