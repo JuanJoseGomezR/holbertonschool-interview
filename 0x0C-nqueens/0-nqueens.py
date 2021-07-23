ebd49ad712e6987c1cf2477796d22508c6b7ffe4
JuanJoseGomezR <juangomez0149@gmail.com>
2021-07-23 00:25:56 -0500

    Update 0-nqueens.py

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,45 +1,63 @@
 #!/usr/bin/python3
-"""N queens program"""
+"""program that solves the N queens problem"""
+
 import sys
 
 
-def is_valid(placed_queens):
-    row = len(placed_queens) - 1
-    for i in range(0, row):
-        diff = abs(placed_queens[i][1] - placed_queens[row][1])
-        if diff == 0 or diff == row - i:
-            return False
-    return True
+def queens(n):
+    """program that solves the N queens problem"""
+    trail = []
+    sets = set()
+    for column in range(n):
+        trail.append([0, column])
+        sets.add(column)
+
+    road = []
+    while trail:
+        [row, column] = trail.pop(0)
+        while road and (row < road[0][0]):
+            road.pop(0)
+        if road and (row == road[0][0]):
+            road[0] = [row, column]
+        else:
+            road.insert(0, [row, column])
+
+        nextsrows = row + 1
+        death = set()
+        for (i, j) in road:
+            death.add(j)
+            distance = nextsrows - i
+            if j - distance >= 0:
+                death.add(j - distance)
+            if j + distance < n:
+                death.add(j + distance)
+
+        safe = sets.difference(death)
+        if not safe:
+            if nextsrows == n:
+                temp = road.copy()
+                temp.reverse()
+                print(temp, flush=True)
+            road.pop(0)
+        else:
+            safe = list(safe)
+            safe.reverse()
+            for position in safe:
+                trail.insert(0, [nextsrows, position])
 
 
-def n_queens(n, row, results, placed_queens=[]):
-    if (row == n):
-        results.append(placed_queens[:])
-    else:
-        for col in range(0, n):
-            placed_queens.append([row, col])
-            if (is_valid(placed_queens)):
-                n_queens(n, row + 1, results)
-            placed_queens.pop()
-
-
-def main():
-    if (len(sys.argv) < 2):
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
+        exit(1)
+
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        exit(1)
+
     try:
         n = int(sys.argv[1])
-    except Exception:
+    except:
         print("N must be a number")
-        sys.exit(1)
-    if (n < 4):
-        print("N must be at least 4")
-        sys.exit(1)
-    results = []
-    n_queens(n, 0, results)
-    for result in results:
-        print(result)
-
-
-if __name__ == "__main__":
-    main()
+        exit(1)
+    queens(n)
