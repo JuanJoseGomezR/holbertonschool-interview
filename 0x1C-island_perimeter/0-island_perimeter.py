d4405cd9e5ee8010086918fc4a6746ebcdea94fa
JuanJoseGomezR <juangomez0149@gmail.com>
2021-11-18 19:35:27 -0500

    [Uploading

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
-""" Island Perimeter
+"""
+0-island_perimeter.py
 """
 
 
+def island_perimeter(grid):
+    """
+    Function that returns the perimeter of the island described in grid
+    """
+    i = 0
+    perimeter = 0
+    while i < len(grid):
+        j = 0
+        while j < len(grid[0]):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i - 1 >= 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i + 1 < len(grid) and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j - 1 >= 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j + 1 < len(grid[0]) and grid[i][j + 1] == 1:
+                    perimeter -= 1
+            j += 1
+        i += 1
+    return perimeter
